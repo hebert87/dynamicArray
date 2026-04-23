834f372e85233a4e979e5f69a8a10be3db439783
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-04-23 11:26:20 -0700

    Add DynamicStringList class w/ boilerplate

---
+++ src/DynamicStringList.java
@@ -0,0 +1,25 @@
+public class DynamicStringList implements StringList {
+    public String get(int index) {
+        return "";
+    }
+
+    public void set(int index, String value) {
+
+    }
+
+    public void add(String value) {
+
+    }
+
+    public String remove(int index) {
+        return "";
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public int capacity() {
+        return 1;
+    }
+}
